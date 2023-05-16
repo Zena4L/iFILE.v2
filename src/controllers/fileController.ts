c7380f79e7f28c976a66d85b8f67f4a985369488
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-05-16 19:13:28 +0000

    cleaning up

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -4,69 +4,8 @@ import { File,IFile } from "../models/fileModel";
 import catchAsync from "../utils/CatchAsync";
 import APIFeatures from "../utils/APIFeatures";
 import AppError from "../utils/AppError";
-import multer, { FileFilterCallback } from 'multer'
-
-const multerStorage = multer.diskStorage({
-    destination: (req: Request, file, cb) => {
-      cb(null, 'public/data');
-    },
-    filename: (req: Request, file, cb) => {
-      //file-id-timestamp.ext
-      const ext = file.mimetype.split('/')[1];
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    },
-  });
-  
-  const multerFilter = (req: Request, file:Express.Multer.File, cb: FileFilterCallback) => {
-    const filetypes = /pdf|jpeg|jpg|png|mp3|mp4|wav|avi|mov/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // Check MIME type
-    const mimetype = filetypes.test(file.mimetype);
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
-  
-  
-  
-  const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter,
-  });
-  
-  export const fileUpload = upload.array('originalname', 10);
 
 
-  
-export const uploadFile: RequestHandler = catchAsync(async (req, res, next) => {
-    const { title, description, fileType } = req.body as IFile;
-    const uploadedFile = req.files;
-
-    // const newFile = await File.create({
-    //   title,
-    //   description,
-    //   fileType,
-    //   fileUrl: req.files[0].filename,
-    //   path: req.files[0].originalname,
-    //   uploadedBy: req.user!.id, // make sure user is defined before accessing its properties
-    // });
-
-
-
-    console.log(req.body);
-    console.log(uploadedFile)
-  
-    res.status(200).json({
-      status: 'success',
-      message: 'file successfully uploaded',
-      data: {
-        // file: newFile,
-      },
-    });
-  });
-  
 
 
 export const getAllFiles:RequestHandler = catchAsync(async (req, res, next) => {
