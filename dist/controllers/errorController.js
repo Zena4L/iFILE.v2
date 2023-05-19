d789beb9c5315e5e3b502f443fada4c31f968154
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-05-19 13:03:06 +0000

    Remove .config file from repository

--- dist/controllers/errorController.js
+++ dist/controllers/errorController.js
@@ -82,7 +82,7 @@ function errorHandler(err, req, res, next) {
         sendErrorDev(err, req, res);
     }
     else if (process.env.NODE_ENV === 'production') {
-        let error = Object.assign({}, err);
+        let error = { ...err };
         error.message = err.message;
         //   if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.name === 'CastError')
